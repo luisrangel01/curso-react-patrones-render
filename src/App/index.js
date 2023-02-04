ad9981d9c4292310e260114e73f0b9a50661b810
Luis Rangel <luis.rangel@telus.com>
2023-02-03 18:16:22 -0600

    feat: components

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
-
+import React from "react";
+// import { TodoProvider } from "../TodoContext";
+// import { AppUI } from "./AppUI";
 
 function App() {
+  const [state, setState] = React.useState("estado compartido");
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+
+      <TodoList>
+        <TodoItems state={state} />
+      </TodoList>
+    </React.Fragment>
   );
 }
+function TodoHeader({ children }) {
+  return <header>{children}</header>;
+}
+function TodoList({ children }) {
+  return <section className="TodoList-container">{children}</section>;
+}
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+function TodoItems({ state }) {
+  return <p>TodoItems: {state}</p>;
+}
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
